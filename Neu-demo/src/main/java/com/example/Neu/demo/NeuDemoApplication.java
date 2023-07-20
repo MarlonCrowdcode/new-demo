280aadf47c852b8e640b9160748480f9256dd31a
MarlonCrowdcode <139772115+MarlonCrowdcode@users.noreply.github.com>
2023-07-20 09:04:43 +0200

    Add files via upload

---
+++ Neu-demo/src/main/java/com/example/Neu/demo/NeuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Neu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuDemoApplication.class, args);
+	}
+
+}
